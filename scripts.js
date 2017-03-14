e8fd383166350f9adc442dd49eaee02a6f08233e
Weronika Musial <jozefinamusial@gmail.com>
2017-03-14 13:01:32 +0100

    poprawione2

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
 function getTriangleArea(a, h) {
   return a > 0 && h > 0 ? a * h / 2 : 'Nieprawidłowe dane';
 }
-
 console.log(getTriangleArea(10, 6))
+
 var triangle1Area = getTriangleArea(10, 15);
-console.log(triangle1Area);
 var triangle2Area = getTriangleArea(10, 20);
-console.log(triangle2Area);
 var triangle3Area = getTriangleArea(-2, 30);
-console.log(triangle3Area);
+
+console.log(
+  triangle1Area,
+  triangle2Area,
+  triangle3Area
+);
